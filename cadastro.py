da3db0ff7501290389a381e352ba5b9f6bdc8fc7
JinMoriKNG <kakarottossj9@gmail.com>
2025-04-29 10:41:30 -0300

    Add files via upload

---
+++ cadastro.py
@@ -0,0 +1,15 @@
+nome = input('Digite seu nome: ')
+idade = int(input('Digite sua idade: '))
+sexo = input('Digite seu sexo (M\F): ')
+rua = input('Informe sua rua: ')
+bairro = input('Informe seu Bairro: ')
+cidade = input('Digite sua Cidade: ')
+
+print('-'*40)
+print(f'''O seu nome é {nome}.
+Sua idade é {idade}.
+Seu sexo é {sexo}.
+Sua rua é {rua}.
+Seu bairro é {bairro}.
+Sua cidade é {cidade}''')
+print('-'*40)
