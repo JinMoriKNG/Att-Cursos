8239b3e93ae48528c9e6280aed76e2aa9e59593b
JinMoriKNG <kakarottossj9@gmail.com>
2025-08-11 09:07:45 -0300

    Add files via upload

---
+++ aula2/tobirama.js
@@ -0,0 +1,10 @@
+function mudarMensagem() {
+    const titulo = document.getElementById("sasuke")
+    titulo.innerHTML = "OLHA ESSA GEOMETRIA"
+
+    const paragrafo = document.getElementById("uchiha")
+    paragrafo.innerHTML = 'From Brazuka'
+
+    const botão = document.getElementById('mudarBotão')
+    botão.innerHTML = "Bebê"
+}
