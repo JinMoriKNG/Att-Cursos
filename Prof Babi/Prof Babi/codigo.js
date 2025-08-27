ab2d26dd8e0f11799dde6f702e96e68ae8f8bdd5
JinMoriKNG <kakarottossj9@gmail.com>
2025-08-27 11:18:10 -0300

    Add files via upload

---
+++ Prof Babi/Prof Babi/codigo.js
@@ -0,0 +1,11 @@
+async function selectMap() {
+        const mapas = [
+            {Bind: "aaaaaa"},
+            {Heaven: "bbbbb"},
+            {Corrode: "ccccc"},
+            {Icebox: "dddddd"}
+        ]
+        for(i=0;i<mapas.length;i++){
+            if(selectMap('Bind')){
+
+}
